d16677d1bae3392ea0f3d40cf10aad2c8e75efa9
Sarah Drake <jussaradrake@outlook.com>
2019-07-07 14:03:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+["am", 2]
+[0
+[2]
 end
 
 def retrieve_first_element_from_array(array)
